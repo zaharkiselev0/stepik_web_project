4077545e1f437574c384b12b1717ffa8ca1df52e
Zahar <zaharkiselev0@gmail.com>
2018-04-11 02:28:10 +0300

    Vech Sh + 1 row in inin.sh

--- web/init.sh
+++ web/init.sh
@@ -1,3 +1,4 @@
+  sudo unlink /etc/nginx/sites-enabled/default
   sudo ln -sf /home/box/web/etc/nginx.conf /etc/nginx/sites-enabled/test.conf
   sudo rm -rf /etc/nginx/sites-enabled/default
   sudo /etc/init.d/nginx restart
