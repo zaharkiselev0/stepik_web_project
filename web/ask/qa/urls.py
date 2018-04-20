4d934eda3963a14e52fedc37fe549d451ebb13a8
zahar <efwfe>
2018-04-20 00:25:57 +0000

    fix another problem in urls.py

--- web/ask/qa/urls.py
+++ web/ask/qa/urls.py
@@ -4,11 +4,11 @@ from qa.views import test
 from qa.q_views import *
 
 urlpatterns = [
- url(r'^$',main,name='main'),
  url(r'^login/$',test,name='test'),
  url(r'^signup/$',test,name='test'),
  url(r'^ask/$',test,name='test'),
  url(r'^new/$',test,name='test'),
  url(r'^popular/$',popular,name='popular'),
  url(r'^question/(?P<id>\d+)/$',question,name='question'),
+ url(r'^$',main,name='main'),
 ]
