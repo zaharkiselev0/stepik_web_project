2570002a3310b69aa9da064ae056e285d192c801
zahar <wegweg>
2018-04-20 02:53:05 +0000

    fix prblem at ask/urls.py

--- web/ask/ask/urls.py
+++ web/ask/ask/urls.py
@@ -17,11 +17,5 @@ from django.conf.urls import url, include
 from django.contrib import admin
 
 urlpatterns = [
-    url(r'^$',include('qa.urls')),
-    url(r'^login/$',include('qa.urls')),
-    url(r'^signup/$',include('qa.urls')),
-    url(r'^ask/$',include('qa.urls')),
-    url(r'^popular/$',include('qa.urls')),
-    url(r'^new/$',include('qa.urls')),
-    url(r'^question/(\d+)/$',include('qa.urls')),
+    url(r'^',include('qa.urls')),
 ]
